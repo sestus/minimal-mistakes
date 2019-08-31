8c45bc854ae05fe0778632fb97f6cd95e669314a
Mike <mike@mikemylonakis.com>
2019-08-31 20:59:22 +0100

    Minor refactoring

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -133,4 +133,16 @@ $(document).ready(function() {
       $(this).append(anchor);
     }
   });
+  $(".language-bash .highlight span.nv").each(function (index) {
+      console.log($(this).text());
+      if ($(this).text() === "$ " || $(this).text() === "# ") {
+          $(this).css({
+                  "user-select": "none",
+                  "-moz-user-select": "none",
+                  "-webkit-user-select": "none",
+                  "-ms-user-select": "none"
+              }
+          )
+      }
+  });
 });
